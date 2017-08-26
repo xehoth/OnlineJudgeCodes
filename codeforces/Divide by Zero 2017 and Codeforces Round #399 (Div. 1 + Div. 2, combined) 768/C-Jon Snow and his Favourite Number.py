654f28623d9bc750b2bb74e750b3007d0d0f2842
xehoth <824547322@qq.com>
2017-08-26 11:10:46 +0800

    Backup 20170826

--- codeforces/Divide by Zero 2017 and Codeforces Round #399 (Div. 1 + Div. 2, combined) 768/C-Jon Snow and his Favourite Number.py
+++
@@ -1,8 +0,0 @@
-n, k, x = map(int, input().split())
-rangers = list(map(int, input().split()))
-for i in range(min(k, 8 + (k & 3))):
-    rangers.sort()
-    rangers = [rangers[i] if (i & 1) else rangers[i] ^ x for i in range(n)]
-    print(rangers)
-rangers.sort()
-print(rangers[-1], rangers[0])
