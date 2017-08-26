d623bb6b967106037f252604283cb2f200d40e9b
flandre495 <3301366379@qq.com>
2017-08-26 11:06:49 +0800

    Backup 20170826

--- codeforces/Divide by Zero 2017 and Codeforces Round #399 (Div. 1 + Div. 2, combined) 768/B-Code For 1.py
+++
@@ -1,7 +0,0 @@
-from math import log2
-n, l, r = map(int, input().split())
-a = list(map(int, list(bin(n)[2:])))
-ans = 0
-for i in range(l, r + 1):
-    ans += a[int(log2(i & -i))]
-print(ans)
--- codeforces/Divide by Zero 2017 and Codeforces Round #399 (Div. 1 + Div. 2, combined) 768/E-Game of Stones.py
+++
@@ -1,4 +0,0 @@
-from math import sqrt
-k = 0
-for t in range(int(input())): k ^= int(int(sqrt(8 * int(input()) + 1) - 1) / 2)
-print("NO" if k != 0 else "YES")
