32d76032cd3a7217027743ae5466a2b0def45002
xehoth <824547322@qq.com>
2017-09-28 15:25:25 +0800

    「BZOJ 4300」绝世好题-DP

---
+++ BZOJ/BZOJ4300-绝世好题-DP.cpp
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2017, xehoth
+ * All rights reserved.
+ * 「BZOJ 4300」绝世好题 28-09-2017
+ * DP
+ * @author xehoth
+ */
+#include <bits/stdc++.h>
+
+namespace IO {
+
+inline char read() {
+    static const int IN_LEN = 1000000;
+    static char buf[IN_LEN], *s, *t;
+    s == t ? t = (s = buf) + fread(buf, 1, IN_LEN, stdin) : 0;
+    return s == t ? -1 : *s++;
+}
+
+template <typename T>
+inline void read(T &x) {
+    static char c;
+    static bool iosig;
+    for (c = read(), iosig = false; !isdigit(c); c = read()) {
+        if (c == -1) return;
+        c == '-' ? iosig = true : 0;
+    }
+    for (x = 0; isdigit(c); c = read()) x = x * 10 + (c ^ '0');
+    iosig ? x = -x : 0;
+}
+
+inline void read(char &c) {
+    while (c = read(), isspace(c) && c != -1)
+        ;
+}
+
+inline int read(char *buf) {
+    register int s = 0;
+    register char c;
+    while (c = read(), isspace(c) && c != -1)
+        ;
+    if (c == -1) {
+        *buf = 0;
+        return -1;
+    }
+    do
+        buf[s++] = c;
+    while (c = read(), !isspace(c) && c != -1);
+    buf[s] = 0;
+    return s;
+}
+
+const int OUT_LEN = 1000000;
+
+char obuf[OUT_LEN], *oh = obuf;
+
+inline void print(char c) {
+    oh == obuf + OUT_LEN ? (fwrite(obuf, 1, OUT_LEN, stdout), oh = obuf) : 0;
+    *oh++ = c;
+}
+
+template <typename T>
+inline void print(T x) {
+    static int buf[30], cnt;
+    if (x == 0) {
+        print('0');
+    } else {
+        x < 0 ? (print('-'), x = -x) : 0;
+        for (cnt = 0; x; x /= 10) buf[++cnt] = x % 10 | 48;
+        while (cnt) print((char)buf[cnt--]);
+    }
+}
+
+inline void print(const char *s) {
+    for (; *s; s++) print(*s);
+}
+
+inline void flush() { fwrite(obuf, 1, oh - obuf, stdout); }
+
+struct InputOutputStream {
+    template <typename T>
+    inline InputOutputStream &operator>>(T &x) {
+        read(x);
+        return *this;
+    }
+
+    template <typename T>
+    inline InputOutputStream &operator<<(const T &x) {
+        print(x);
+        return *this;
+    }
+
+    ~InputOutputStream() { flush(); }
+} io;
+}
+
+namespace {
+
+using IO::io;
+
+int f[32];
+inline void solve() {
+    register int n, ans = 0;
+    io >> n;
+    for (register int i = 1, x, tmp; i <= n; i++) {
+        io >> x, tmp = 0;
+        for (register int j = 0; j <= 30; j++)
+            (x & (1 << j)) ? tmp = std::max(tmp, f[j] + 1) : 0;
+        for (register int j = 0; j <= 30; j++) (x & (1 << j)) ? f[j] = tmp : 0;
+        ans = std::max(ans, tmp);
+    }
+    io << ans;
+}
+}
+
+int main() {
+    // freopen("sample/1.in", "r", stdin);
+    solve();
+    return 0;
+}
