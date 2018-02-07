6a492504d00a5314af44b4b67e847a3f5d85a25e
xehoth <824547322@qq.com>
2018-02-07 15:14:08 +0800

    「BZOJ 4724」Podzielno

--- workspace/src/main.cpp
+++ workspace/src/main.cpp
@@ -1,22 +1,92 @@
-#include <bits/stdc++.h>
+/**
+ * Copyright (c) 2016-2018, xehoth
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * 「BZOJ 4724」Podzielno 07-02-2018
+ *
+ * @author xehoth
+ */
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <vector>
 
-const int MAXN = 100000;
-const int MOD = 1e9 + 7;
+struct InputOutputStream {
+    enum { SIZE = 1 << 18 | 1 };
+    char ibuf[SIZE], *s, *t, obuf[SIZE], *oh;
 
-int a[MAXN], b[MAXN], c[MAXN];
+    InputOutputStream() : s(), t(), oh(obuf) {}
 
-int f(int i, int j, int x) {
-    return c[i] < j ? ((unsigned long long)a[i] * x + b[i]) % MOD
-                    : ((unsigned long long)a[i] * f(c[i], j, x) + b[i]) % MOD;
-}
+    ~InputOutputStream() { fwrite(obuf, 1, oh - obuf, stdout); }
+
+    inline char read() {
+        return (s == t) && (t = (s = ibuf) + fread(ibuf, 1, SIZE, stdin)),
+               s == t ? -1 : *s++;
+    }
+
+    template <typename T>
+    inline InputOutputStream &operator>>(T &x) {
+        static char c;
+        static bool iosig;
+        for (c = read(), iosig = false; !isdigit(c); c = read()) {
+            if (c == -1) return *this;
+            iosig |= c == '-';
+        }
+        for (x = 0; isdigit(c); c = read()) x = x * 10 + (c ^ '0');
+        iosig && (x = -x);
+        return *this;
+    }
+
+    inline void print(char c) {
+        (oh == obuf + SIZE) && (fwrite(obuf, 1, SIZE, stdout), oh = obuf);
+        *oh++ = c;
+    }
+
+    template <typename T>
+    inline void print(T x) {
+        static int buf[21], cnt;
+        if (x != 0) {
+            (x < 0) && (print('-'), x = -x);
+            for (cnt = 0; x; x /= 10) buf[++cnt] = x % 10 | 48;
+            while (cnt) print((char)buf[cnt--]);
+        } else {
+            print('0');
+        }
+    }
+
+    template <typename T>
+    inline InputOutputStream &operator<<(const T &x) {
+        print(x);
+        return *this;
+    }
+} io;
+
+int n, q, sum;
 
 int main() {
-    int n, m;
-    std::cin >> n >> m;
-    for (int i = 1; i <= n; i++) std::cin >> a[i] >> b[i] >> c[i];
-    for (int i, j, x; m--;) {
-        std::cin >> i >> j >> x;
-        std::cout << f(i, j, x) << '\n';
+    io >> n >> q;
+    std::vector<long long> a(n);
+    for (int i = 0; i < n; i++) {
+        io >> a[i];
+        sum = (sum + a[i] * i) % (n - 1);
+    }
+    if (sum) a[sum]--;
+    for (int i = 1; i < n; i++) a[i] += a[i - 1];
+    long long tmp;
+    for (std::vector<long long>::iterator ans; q--;) {
+        io >> tmp;
+        ans = std::upper_bound(a.begin(), a.end(), tmp);
+        io << (ans != a.end() ? ans - a.begin() : -1) << '\n';
     }
     return 0;
 }
