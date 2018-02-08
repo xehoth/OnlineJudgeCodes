41979b7fc6154ad36621b75b3efb4cada79657a7
xehoth <824547322@qq.com>
2018-02-08 16:08:57 +0800

    「SuperOJ 1693」Alice的梦境-KM

--- workspace/test/data.cpp
+++ workspace/test/data.cpp
@@ -1,10 +1,9 @@
 #include <bits/stdc++.h>
 
 int main() {
-    int n = 500;
-    std::cout << n << '\n';
-    srand(time(0));
-    for (int i = 1; i <= 4 * n * (n + 1); i++) {
-        std::cout << rand() % 1000001 << '\n';
+    int n = 400;
+    std::cout << n 
+    for (int i = 1; i <= n; i++) {
+
     }
 }
