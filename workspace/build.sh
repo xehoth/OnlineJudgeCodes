8969f1e5503e565cf28094887345e3677b2a10c2
xehoth <824547322@qq.com>
2018-06-01 20:49:46 +0800

    update

--- workspace/build.sh
+++ workspace/build.sh
@@ -2,4 +2,4 @@
 # rm "CMakeCache.txt" "cmake_install.cmake" "Makefile"
 # rm -r "CMakeFiles"
 # cmake . && make
-g++-7 -g src/main.cpp -o main -DDBG
+g++-7 -g src/main.cpp -o main -DDBG -D_DEBUG -Wall -Wextra
