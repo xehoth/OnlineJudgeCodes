dee4b78c4fd9e0d75ebf120baca501983cfae3b8
xehoth <824547322@qq.com>
2018-06-01 20:49:38 +0800

    update

--- workspace/run.sh
+++ workspace/run.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
+timeLimit=2
 red='\033[0;31m'
 green='\033[0;32m'
 reset='\033[0m'
 cyan='\033[0;36m'
 
-./build.sh
 cd sample
 cat /dev/null > diff.log
 for i in $(ls | grep '\.in' | sed 's/\.in//g'); do 
-    time ../main < $i.in > out.out; 
+# if ! timeout $timeLimit time -v ../main < $i.in > out.out; then
+    if ! timeout $timeLimit time -f "%Es %Mkb\n" ../main < $i.in > out.out; then
+        echo -e ${cyan}"TimeLimitExceeded on test $i.1"${reset};
+        if ! timeout $timeLimit time -f "%Es %Mkb\n" ../main < $i.in > out.out; then
+            echo -e ${cyan}"TimeLimitExceeded on test $i.2"${reset};
+            if ! timeout $timeLimit time -f "%Es %Mkb\n" ../main < $i.in > out.out; then
+                echo -e ${cyan}"TimeLimitExceeded on test $i.3"${reset};
+                echo -e ${cyan}"TimeLimitExceeded on test $i"${reset};
+                continue;
+            fi
+        fi
+    fi
     if diff -Bb out.out $i.out >> diff.log; then
         echo -e ${green}"Sample $i Accepted!"${reset};
     else
--- workspace/test/data.cpp
+++ workspace/test/data.cpp
@@ -1,17 +1,41 @@
 #include <bits/stdc++.h>
 
+#include <bits/stdc++.h>
+
+#ifdef __linux__
+unsigned long GetTickCount() {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return (ts.tv_sec * 1000 + ts.tv_nsec / 1000000);
+}
+#else
+#include <Windows.h>
+#endif
+
 int main() {
-    int k;
-    std::cin >> k;
-    int S = 0;
-    int T = k;
-    for (int i = 0; i < k - 1; k++)
-        std::cout << i + 1 << ' ' << i + 2 << ' ' << k << '\n';
-    for (int i = S; i < k; i++) {
-        std::cout << i << ' ' << i + 1 << ' ' << 
-    }
-    for (int i = 1; i <= n; i++) {
-        
-    }
+    char *seed = new char;
+    srand((long)seed ^ time(0) ^ GetTickCount());
+    delete seed;
     return 0;
-} 
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    char *seed = new char;
+    std::mt19937 engine(time(0) | (long)seed);
+    delete seed;
+    int n = 180, c = 50, m = 40000;
+    std::uniform_int_distribution<> dis(1, 1000000000);
+    auto gen = std::bind(dis, engine);
+    std::cout << n << ' ' << m << ' ' << c << '\n';
+    for (int i = 1; i <= m; i++)
+        std::cout << engine() % n + 1 << ' ' << engine() % n + 1 << ' ' << engine() % c + 1 << '\n';
+    std::vector<int> w;
+    for (int i = 1; i < 2; i++) w.push_back(0);
+    for (int i = 2; i <= c; i++) w.push_back(gen());
+    std::sort(w.begin(), w.end());
+    for (int v : w) std::cout << v << ' ';
+    return 0;
+}
